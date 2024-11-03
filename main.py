81e5c61739a0ef692d70ba06c9e29d6fdc72af74
theorak <proensch@prdev.eu>
2024-11-03 10:52:53 +0100

    added screen (windowed) and game loop with quit event

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill(000000)
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
